3c33bdb69638720f77a45fec3b4f6ad6493b0b04
Vinycius Jakson <jaksonvinycius@gmail.com>
2026-06-26 15:54:15 +0100

    fix: update GTM container ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MVS3K72J');`,
+})(window,document,'script','dataLayer','GTM-5ZRX7VJ8');`,
           }}
         />
       </head>
@@ -79,7 +79,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MVS3K72J"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5ZRX7VJ8"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
